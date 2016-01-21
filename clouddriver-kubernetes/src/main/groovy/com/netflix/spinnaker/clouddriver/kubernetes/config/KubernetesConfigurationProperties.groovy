b7f0147181b9a6e746cae427f07ea41839eeafcb
Lars Wander <lwander@google.com>
2016-01-21 10:14:16 -0500

    Added docker credentials

--- clouddriver-kubernetes/src/main/groovy/com/netflix/spinnaker/clouddriver/kubernetes/config/KubernetesConfigurationProperties.groovy
+++ clouddriver-kubernetes/src/main/groovy/com/netflix/spinnaker/clouddriver/kubernetes/config/KubernetesConfigurationProperties.groovy
@@ -25,10 +25,31 @@ class KubernetesConfigurationProperties {
     String name
     String environment
     String accountType
+    // Kubernetes endpoint address.
     String master
+    // Username required for authenticating with Kubernetes endpoint.
     String username
+    // Password required for authenticating with Kubernetes endpoint.
     String password
+    // (Default = ['default']) Namespaces to be managed.
     List<String> namespaces
+    // Credentials for authenticating with Docker Registry (must conform to v2 API).
+    List<DockerRegistry> dockerRegistry
+
+    @ToString(includeNames = true)
+    static class DockerRegistry {
+      // Docker registry username.
+      String username
+      // Docker registry password.
+      String password
+      // Docker registry user email address.
+      String email
+      // URL of the registry.
+      String url
+      // List of all repositories to index. Can be of the form <user>/<repo>,
+      // or <library> for repositories like 'ubuntu'.
+      List <String> repositories
+    }
   }
 
   List<ManagedAccount> accounts = []
